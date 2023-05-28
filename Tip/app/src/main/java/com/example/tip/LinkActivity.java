f2539180b561781171ec69ec6c13d98cafa89328
유진호 <u06930@naver.com>
2023-05-28 20:25:40 +0900

    금연팁

---
+++ Tip/app/src/main/java/com/example/tip/LinkActivity.java
@@ -0,0 +1,35 @@
+package com.example.tip;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LinkActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.linkactivitiy_main);
+        webView = findViewById(R.id.webView);
+        String url = getIntent().getStringExtra("url");
+        if (url != null) {
+
+            webView.loadUrl(url);
+        }
+
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+    }
+}
