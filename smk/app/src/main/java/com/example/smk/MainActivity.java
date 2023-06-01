dcf38ed3ba8dbbd47db2b1d20cc1a0822811a690
유진호 <u06930@naver.com>
2023-06-02 07:27:26 +0900

    니코틴 중독 자가진단

---
+++ smk/app/src/main/java/com/example/smk/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.smk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button imageButton = (Button) findViewById(R.id.btn1);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SmokeTest.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
