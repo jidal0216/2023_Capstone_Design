e3d3ecd468b2950ea9a401795acea1bbff958384
유진호 <u06930@naver.com>
2023-06-06 20:29:06 +0900

    텍스트 순차적 출력

---
+++ MyApplication21/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tomer.fadingtextview.FadingTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // String[] texts = {"text1","text2","text3"};
+
+        FadingTextView FTV = (FadingTextView) findViewById(R.id.fadingTextView);
+        // FTV.setTexts(texts); //You can use an array resource or a string array as the parameter
+        FTV.setTimeout(10, SECONDS);
+    }
+}
